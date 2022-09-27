813f7dd9f2c4c04da0c3802d6ae15f1658a39b46
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2022-09-27 19:51:05 +0100

    complete

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
+    for i in items:
+        i = str(i)
+        if(i in frequencies.keys()):
+            frequencies[i] +=1
+
+        else:
+            frequencies[i] = 1
     # Your code goes here
     return frequencies
