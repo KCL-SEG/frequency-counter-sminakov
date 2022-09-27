a62c1927a5d6e4998f9a7b00a3b3c31e08bf38aa
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2022-09-27 21:58:01 +0100

    change algorithm

--- frequencies.py
+++ frequencies.py
@@ -6,12 +6,12 @@ list = ['a', 'a', 'b', 'b', 'b', 'c']
 def frequencies(items):
     frequencies = {}
     for i in items:
-        i = str(i)
-        if(i in frequencies.keys()):
-            frequencies[i] +=1
+        current = str(i)
+        if(current in frequencies.keys()):
+            frequencies[current] +=1
 
         else:
-            frequencies[i] = 1
+            frequencies[current] = 1
     # Your code goes here
     return frequencies
 
